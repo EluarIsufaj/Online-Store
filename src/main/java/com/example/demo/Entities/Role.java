53dea054b0f3f3a19126ecd5303aeb9f8ff43bd4
EluarIsufaj <eluarisufaj2@gmail.com>
2024-02-03 16:23:26 +0100

    Entitetet e para (User, Role) jane shtuar ne projekt.

---
+++ src/main/java/com/example/demo/Entities/Role.java
@@ -0,0 +1,14 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class Role {
+
+    private String roleName;
+
+
+}
---
+++ src/main/java/com/example/demo/Entities/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String email;
+    private String password;
+    private String City;
+    private String address;
+    private byte[] logo;
+    private Role role;
+
+
+
+}
