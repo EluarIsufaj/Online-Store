8be300a2431ecd2f991bfe03ce13e51f459feb72
User <eluariufaj2@gmail.com>
2024-02-14 15:49:43 +0100

    Update Notes: Bug Fixes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan
 @SpringBootApplication
 @EntityScan(basePackages = "com.example.demo.Entities")
 public class DemoApplication {
